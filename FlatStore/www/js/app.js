133d4a6db18625cb385ab327b222fd200e9667d8
Bramver <Bramvereertbrugghen@outlook.com>
2016-04-04 15:14:09 +0200

    Updated index, finished app.js and starting on views now.

--- FlatStore/www/js/app.js
+++ FlatStore/www/js/app.js
@@ -1,10 +1,81 @@
 (function() {
   var app = angular.module('gemStore', ['ionic']);
 
+
   //CONTROLLERS
   app.controller('StoreController', function() {
-    this.product = gems;
+    this.products = gems;
   });
+ 
+  app.controller("ReviewController", function(){
+ 
+    this.review = {};
+ 
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+ 
+  });
+ 
+  app.directive("productDescriptions", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "views/product-description.html"
+    };
+  });
+
+
+
+  //DIRECTIVES
+  app.directive('productGallery', function() {
+    return {
+      restrict: 'E',
+      templateUrl: 'views/product-gallery.html',
+      controller: function() {
+        this.current = 0;
+        this.setCurrent = function(imageNumber){
+          this.current = imageNumber || 0;
+        };
+      },
+      controllerAs: 'gallery'
+    };
+  });
+ 
+  app.directive("productReviews", function() {
+    return {
+      restrict: 'E',
+      templateUrl: "views/product-reviews.html"
+    };
+  });
+ 
+  app.directive("productSpecs", function() {
+    return {
+      restrict:"A",
+      templateUrl: "views/product-specs.html"
+    };
+  });
+ 
+  app.directive("productTabs", function() {
+    return {
+      restrict: "E",
+      templateUrl: "views/product-tabs.html",
+      controller: function() {
+        this.tab = 1;
+ 
+        this.isSet = function(checkTab) {
+          return this.tab === checkTab;
+        };
+ 
+        this.setTab = function(activeTab) {
+          this.tab = activeTab;
+        };
+      },
+      controllerAs: "tab"
+    };
+  });
+ 
+
 
   //VARIABLES
   var gems = [
@@ -12,6 +83,7 @@
       name: 'Azurite',
       description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
       shine: 8,
+      date: '2011-03-25',
       price: 110.50,
       rarity: 7,
       color: '#CCC',
@@ -34,6 +106,7 @@
       name: 'Bloodstone',
       description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
       shine: 9,
+      date: '2015-11-14',
       price: 22.90,
       rarity: 6,
       color: '#EEE',
@@ -56,6 +129,7 @@
         name: 'Zircon',
         description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
         shine: 70,
+      date: '1998-02-08',
         price: 1100,
         rarity: 2,
         color: '#000',
